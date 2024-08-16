4164ceae069a5a255b3fcd0f507eeccb09019660
Love <rr1150650@gmail.com>
2024-08-16 22:34:53 +0530

    Started building header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-   <Navbar />
-  )
-}
+    <div className="flex flex-row ">
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="text-red-700 bg-blue-500 text-4xl">Header</div>;
+};
+
+export default Header;
