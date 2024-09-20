99c98e02c34050615993b3c65156a3915c20af91
Love <rr1150650@gmail.com>
2024-09-20 21:03:33 +0530

    setup database(MongoDB) with mongoose

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb://localhost:27017/connect', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
